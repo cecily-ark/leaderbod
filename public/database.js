03ff4c60139f74d341a44edbb04c788e02a8bae5
Emmanuel <cecilarchibong9@gmail.com>
2023-02-19 00:38:45 +0100

    first commit

---
+++ public/database.js
@@ -0,0 +1,142 @@
+export const Leaderboard = [
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/ images/one.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/two.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: three.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: four.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: fivejpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: six.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: seven.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: eight.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: nine.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: ten.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: eleven.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: twelve.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: thirteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: fourteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: fifteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: sixteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: seventeen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: eighteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: ninteen.jpg,
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: twenty.jpg,
+        dt: "2023-02-10"
+    },
+]
---
+++ src/components/database.js
@@ -0,0 +1,142 @@
+export const Leaderboard = [
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/ images/one.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/two.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/three.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/four.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/five.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/six.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/seven.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/eight.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/nine.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/ten.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/eleven.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/twelve.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/thirteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/fourteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/fifteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/sixteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/seventeen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/eighteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/ninteen.jpg",
+        dt: "2023-02-10"
+    },
+    {
+        name: "Shawn Hanna",
+        location: "India",
+        score: 1550,
+        img: "/images/twenty.jpg",
+        dt: "2023-02-10"
+    },
+]
